ce8e0e6b637eaee201a64be6aa7da1f67aa1114e
Bartłomej Litwin <testowe061@gmail.com>
2025-06-11 21:26:06 +0200

    mini-parser: Many changes

--- mini-parser.zig
+++ mini-parser.zig
@@ -2,35 +2,41 @@ const std = @import("std");
 const mem = std.mem;
 
 pub const Table = struct {
-    name: []const u8,
-    short_name: u8,
+    long: []const u8,
+    short: u8,
     type: enum { argument, boolean },
 };
 
-const ArgumentData = struct {
-    argument: usize,
-    value: []const u8,
+const ArgData = struct {
+    arg: usize,
+    val: []const u8,
 };
 
-pub fn init(Arg: [*:0]u8, comptime table: []const Table) !ArgumentData {
-    const args = mem.sliceTo(Arg, 0);
+pub fn init(index: usize, comptime table: []const Table) !ArgData {
     const argv = std.os.argv;
+    const args = mem.sliceTo(argv[0..][index], 0);
 
-    if (argv.len == 1) return .{ .argument = 0, .value = "" };
-
-    var i: usize = 1;
-    while (table.len > i) : (i += 1) {
-        inline for (table, 1..table.len + 1) |flag, arg| {
-            const argument = mem.eql(u8, "--" ++ flag.name, args);
-            const short_argument = mem.eql(u8, &[_]u8{ '-', flag.short_name }, args);
-
-            if (argument or short_argument) return switch (flag.type) {
-                .boolean => .{ .argument = arg, .value = "" },
-                .argument => .{ .argument = arg, .value = mem.sliceTo(Arg[args.len + 1 ..], 0) },
-            };
-        }
-        if (mem.eql(u8, args[0..1], "--") or args[0] == '-') return .{ .argument = table.len + 1, .value = "" };
+    if (argv.len == 1) {
+        return .{ .arg = 0, .val = "" };
     }
 
-    return .{ .argument = table.len + 2, .value = "" };
+    for (1..table.len) |_| {
+        inline for (table, 1..table.len + 1) |flag, arg| {
+            const argument = mem.eql(u8, "--" ++ flag.long, args);
+            const short_argument = mem.eql(u8, &[2]u8{ '-', flag.short }, args);
+
+            if (argument or short_argument) {
+                return switch (flag.type) {
+                    .boolean => .{ .arg = arg, .val = undefined },
+                    .argument => .{ .arg = arg, .val = mem.span(argv[index + 1]) },
+                };
+            }
+        }
+
+        if (args[0] == '-' or mem.eql(u8, args[0..1], "--")) {
+            return .{ .arg = table.len + 1, .val = mem.span(argv[index]) };
+        }
+    }
+
+    return .{ .arg = table.len + 2, .val = undefined };
 }

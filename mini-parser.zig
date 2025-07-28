61adf9edfd755fd2f3e2f0fd71cc8fe5e3aa517e
Bartłomej Litwin <testowe061@gmail.com>
2025-07-28 22:55:46 +0200

    mini-parser: Small overhaul
    - Add multi-argument support and 'args' variable
    - Make the '=' character usable
    - Seperate argument comparison to 'compare_argument()' and argument values to 'argument_value()'
    - Return value when flag is not provided e.g './foo <value>'

--- mini-parser.zig
+++ mini-parser.zig
@@ -7,36 +7,81 @@ pub const Table = struct {
     type: enum { argument, boolean },
 };
 
-const ArgData = struct {
-    arg: usize,
-    val: []const u8,
-};
-
-pub fn init(index: usize, comptime table: []const Table) !ArgData {
+pub fn argument_value(argument: []const u8, argv_index: usize) []const u8 {
     const argv = std.os.argv;
-    const args = mem.sliceTo(argv[0..][index], 0);
+    var index: usize = 0;
 
-    if (argv.len == 1) {
-        return .{ .arg = 0, .val = "" };
+    while (index < argument.len) : (index += 1) {
+        switch (argument[index]) {
+            '=' => return @constCast(&mem.splitBackwardsAny(u8, argument, "=")).first(),
+            else => {},
+        }
     }
 
-    for (1..table.len) |_| {
-        inline for (table, 1..table.len + 1) |flag, arg| {
-            const argument = mem.eql(u8, "--" ++ flag.long, args);
-            const short_argument = mem.eql(u8, &[2]u8{ '-', flag.short }, args);
+    return mem.span(argv[argv_index + 1]);
+}
 
-            if (argument or short_argument) {
-                return switch (flag.type) {
-                    .boolean => .{ .arg = arg, .val = undefined },
-                    .argument => .{ .arg = arg, .val = mem.span(argv[index + 1]) },
+pub fn compare_argument(args: usize, argument: []const u8, comptime flag: Table) struct {
+    args: usize,
+    bool: bool,
+} {
+    if (argument[0] != '-') return .{ .args = args, .bool = false };
+
+    const flag_argument = if (flag.type != .boolean)
+        @constCast(&mem.splitAny(u8, argument, "=")).first()
+    else
+        argument;
+
+    if (!mem.startsWith(u8, flag_argument, "--")) {
+        for (args..flag_argument.len) |index| {
+            if (flag_argument[index] == flag.short) {
+                return .{
+                    .args = if (index + 1 != flag_argument.len)
+                        index + 1
+                    else
+                        index,
+                    .bool = true,
+                };
+            }
+        }
+    }
+
+    return .{ .args = args, .bool = mem.eql(u8, "--" ++ flag.long, flag_argument) };
+}
+
+pub fn init(index: usize, args: usize, comptime table: []const Table) !struct {
+    arg: u8,
+    val: []const u8,
+    args: usize,
+} {
+    const argv = std.os.argv;
+    const argument = mem.sliceTo(argv[0..][index], 0);
+
+    if (argv.len <= 1) {
+        return .{ .arg = 0, .val = undefined, .args = args };
+    }
+
+    var i: usize = 0;
+    while (i < table.len) : (i += 1) {
+        inline for (table) |flag| {
+            const comparison = compare_argument(args, argument, flag);
+
+            if (comparison.bool) {
+                return .{
+                    .arg = flag.short,
+                    .val = switch (flag.type) {
+                        .boolean => undefined,
+                        .argument => argument_value(argument, index),
+                    },
+                    .args = comparison.args,
                 };
             }
         }
 
-        if (args[0] == '-' or mem.eql(u8, args[0..1], "--")) {
-            return .{ .arg = table.len + 1, .val = mem.span(argv[index]) };
+        if (argument[0] == '-' or mem.startsWith(u8, argument, "--")) {
+            return .{ .arg = 1, .val = mem.span(argv[index]), .args = args };
         }
     }
 
-    return .{ .arg = table.len + 2, .val = undefined };
+    return .{ .arg = 2, .val = mem.span(argv[index]), .args = args };
 }
